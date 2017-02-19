c81f07d2c2532d542f49275531e286dc4c660145
babyron <babyron64@gmail.com>
2017-02-19 17:17:28 +0900

    bug fix

--- install.sh
+++ install.sh
@@ -1,9 +1,18 @@
 #! /bin/bash
 
 ins_dir=$(pwd)
-cd ../
-mv ins_dir $HOME
-cd $HOME
-mv ins_dir .vim
-cd .vim
+if [ -e $HOME/.vimrc ] 
+then
+	rm $HOME/.vimrc
+fi
+if [ -e $HOME/.vim ] 
+then
+	rm -rf $HOME/.vim
+fi
 cp ./.vimrc $HOME/.vimrc
+cd ../
+cp -r $ins_dir $HOME/.vim
+mkdir -p $HOME/.vim/dein/repos/github.com/Shougo/dein.vim
+git clone https://github.com/Shougo/dein.vim.git \
+	$HOME/.vim/dein/repos/github.com/Shougo/dein.vim
+
